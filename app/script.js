fa68bc12c8dfc6ceff4f53f7959a96a8c43f0d52
melnyksan <andriimelnyk986@gmail.com>
2019-09-20 21:01:19 +0300

    Test task is done

--- app/script.js
+++ app/script.js
@@ -1,4 +1,18 @@
-function greet() {
-    document.getElementById('greeting').innerHTML = 'Hi, task is done below.';
-   }
+const fruits = ['Banana', 'Apple', 'Orange', 'Lemon'];
 
+const match = 'Apple';
+const newItem = 'Only'.concat(' ', match);
+const index = fruits.findIndex(item => item === match);
+
+fruits.splice(index, 1, newItem);
+
+const list = document.createElement('ol');
+
+fruits.forEach(function(fruit){
+    const li = document.createElement('li');
+    li.textContent = fruit;
+    list.appendChild(li);
+});
+
+const app = document.querySelector('#app');
+app.appendChild(list);
